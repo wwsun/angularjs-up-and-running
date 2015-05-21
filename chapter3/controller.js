c5f3bac1d26417268c6eb78d8ffd1541bbc993d8
Weiwei SUN <ww.sun@outlook.com>
2015-05-21 14:16:51 +0800

    update ch3 codes

--- chapter3/controller.js
+++ chapter3/controller.js
@@ -1,17 +1,17 @@
 // File: chapter3/controller.js
 angular.module('notesApp', [])
-  .controller('ListCtrl', [function() {
+    .controller('ListCtrl', [function () {
 
-    var self = this;
-    self.items = [
-      {id: 1, label: 'First', done: true},
-      {id: 2, label: 'Second', done: false}
-    ];
+        var self = this;
+        self.items = [
+            {id: 1, label: 'First', done: true},
+            {id: 2, label: 'Second', done: false}
+        ];
 
-    self.getDoneClass = function(item) {
-      return {
-        finished: item.done,
-        unfinished: !item.done
-      };
-    };
-}]);
+        self.getDoneClass = function (item) {
+            return {
+                finished: item.done,
+                unfinished: !item.done
+            };
+        };
+    }]);
